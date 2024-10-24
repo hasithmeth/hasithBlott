b93015bcd4b2ddf5f6c15bae13f584139ae45e2d
Hasith Jayasekara <hasith.jayasekara@edsmart.com>
2024-10-25 01:39:55 +0530

    [chore] added dependencies and setup env

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    'module:react-native-dotenv',
+    'react-native-reanimated/plugin',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          'moti/skeleton': 'moti/skeleton/react-native-linear-gradient',
+        },
+      },
+    ],
+  ],
 };
