037f84e628e02c4193e762ac2a70574148239e68
Hasith Jayasekara <hasith.jayasekara@edsmart.com>
2024-10-25 02:39:52 +0530

    [chore] splash screens added

---
+++ src/@types/fonts.d.ts
@@ -0,0 +1,14 @@
+interface Fonts {
+  regular400: string;
+  regularItalic400: string;
+  thin100: string;
+  thinItalic100: string;
+  light300: string;
+  lightItalic300: string;
+  medium500: string;
+  mediumItalic500: string;
+  bold700: string;
+  boldItalic700: string;
+  black900: string;
+  blackItalic900: string;
+}
---
+++ src/assets/fonts/index.ts
@@ -0,0 +1,14 @@
+export const FONT_FAMILY: Fonts = {
+  regular400: 'Roboto-Regular',
+  regularItalic400: 'Roboto-Italic',
+  thin100: 'Roboto-Thin',
+  thinItalic100: 'Roboto-ThinItalic',
+  light300: 'Roboto-Light',
+  lightItalic300: 'Roboto-LightItalic',
+  medium500: 'Roboto-Medium',
+  mediumItalic500: 'Roboto-MediumItalic',
+  bold700: 'Roboto-Bold',
+  boldItalic700: 'Roboto-BoldItalic',
+  black900: 'Roboto-Black',
+  blackItalic900: 'Roboto-BlackItalic',
+};
