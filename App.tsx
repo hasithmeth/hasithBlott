d6fe60210857989ca7b8fb862df580f06e58e7f4
Hasith Jayasekara <hasith.jayasekara@edsmart.com>
2024-10-25 02:46:05 +0530

    [feat] bootsplash hide added

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import BootSplash from 'react-native-bootsplash';
 
 function App(): JSX.Element {
+  useEffect(() => {
+    const hideSplash = async () => {
+      await BootSplash.hide({fade: true});
+    };
+    hideSplash();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>Hello Blott</Text>
